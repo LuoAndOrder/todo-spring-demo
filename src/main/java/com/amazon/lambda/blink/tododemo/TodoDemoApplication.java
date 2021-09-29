cc45e593a7091c3194172bdad898ac6b1cca4bb3
Kevin Luo <kwluo90@gmail.com>
2021-09-28 20:28:33 -0700

    first commit

---
+++ src/main/java/com/amazon/lambda/blink/tododemo/TodoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.amazon.lambda.blink.tododemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amazon/lambda/blink/tododemo/TodoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazon.lambda.blink.tododemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
